5b003ac13d5d15e1d42b8b6f2d9d2560e75ce017
babyanonymouse <babyanonymouse2@gmail.com>
2024-10-30 09:18:23 +0300

    setup && first commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+//components
+import Socials from "./Socials";
+import Logo from "../img/header/logo.svg";
+import MobileNav from "./MobileNav";
+// link
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="bg-pink fixed w-full px-[30p] lg:px-[100px]">Header</div>
+  );
+};
+
+export default Header;
